7c1917f4cef5ad62ad9c000d0454a37cdb213582
Doğan Can Yelkarası <113992684+yelkarasidogancan@users.noreply.github.com>
2023-07-31 10:51:41 +0300

    datatables

--- main.js
+++ main.js
@@ -1,129 +1,144 @@
-//import './style.css';
-import Map from "./libs/Map.js";
-import OSM from "./libs/source/OSM.js";
-import TileLayer from "./libs/layer/Tile.js";
-import View from "./libs/View.js";
-import Feature from "/libs/Feature.js";
 import "https://code.jquery.com/jquery-3.6.0.min.js";
 import "https://cdn.datatables.net/1.11.4/js/jquery.dataTables.min.js";
 
-var markerCoordinates = [34.145435719887075, 39.40466275924388]; // Longitude, Latitude
+var markerCoordinates = [34.145435719887075, 39.40466275924388];
 
-var map = new ol.Map({
+// new interaction
+const raster = new ol.layer.Tile({
+  source: new ol.source.OSM(),
+});
+
+const source = new ol.source.Vector();
+const vector = new ol.layer.Vector({
+  source: source,
+  style: {
+    "fill-color": "rgba(255, 255, 255, 0.2)",
+    "stroke-color": "#ffcc33",
+    "stroke-width": 2,
+    "circle-radius": 7,
+    "circle-fill-color": "#ffcc33",
+  },
+});
+
+const extent = ol.proj.get("EPSG:3857").getExtent().slice();
+extent[0] += extent[0];
+extent[2] += extent[2];
+const map = new ol.Map({
+  layers: [raster, vector],
   target: "map",
-  layers: [
-    new ol.layer.Tile({
-      source: new ol.source.OSM(),
-    }),
-  ],
   view: new ol.View({
     center: ol.proj.fromLonLat(markerCoordinates),
     zoom: 7,
+    extent,
   }),
 });
-
-// Add Point
+var sourceFeature = "";
+let handler = function (event) {
+  source.removeFeature(sourceFeature);
+};
+document.addEventListener("jspanelcloseduser", handler, false);
 const addPointButton = document.getElementById("addPointBtn");
+const form = document.getElementById("form");
 addPointButton.addEventListener("click", function () {
-  const mapElement = map.getTargetElement();
-  mapElement.style.cursor = "crosshair";
+  form.style.display = "block";
+  const modify = new ol.interaction.Modify({ source: source });
+  map.addInteraction(modify);
 
-  // Tıklama olayını dinleme
-  map.once("click", function (event) {
-    mapElement.style.cursor = "auto";
-    const clickedCoordinate = event.coordinate;
-    var clickedLonLat = ol.proj.toLonLat(clickedCoordinate);
-    console.log(clickedLonLat[0] + "-" + clickedLonLat[1]);
+  let draw, snap;
 
-    let buildingName = "";
-    // using a few options
-    const panel = jsPanel.create({
-      // Panelin içeriği (HTML içeriği)
-      theme: "primary",
-      borderRadius: "1rem",
-      boxShadow: 5,
+  const typeSelect = document.getElementById("type");
 
-      content: `
-          <div class="form">
-            <div class="input-container">
-              <label>Langitude Value </label>
-              <input type="text" id="input1" placeholder="Input 1" readonly>
-            </div>
-            <div class="input-container">
-            <label>Latitude Value </label>
-              <input type="text" id="input2" placeholder="Input 2" readonly>
-            </div>
-            <div class="input-container">
-              <label>Enter Door Name</label>
-              <input type="text" id="buildingNameInput" >
-            </div>
-             <div class="buttonss">
-               <div id="finishButton" class="b">Done</div>
-               <div id="closePanelButton" class="b r">Exit</div>
-             </div>
-            
-          </div>
-      `,
-      contentSize: "700 550",
-      // Panelin özellikleri
-      headerTitle: "Add Point",
-      position: "center",
-      // Diğer isteğe bağlı özellikleri buraya ekleyebilirsiniz
-      // Örn: size, theme, dragit, resizeit vb.
-      callback: function () {
-        // clickedCoordinate verisini inputların içine yazın
-        document.getElementById("input1").value = clickedLonLat[0];
-        document.getElementById("input2").value = clickedLonLat[1];
-
-        document
-          .getElementById("closePanelButton")
-          .addEventListener("click", function () {
-            console.log("sa");
-            panel.close();
-          });
-
-        document
-          .getElementById("finishButton")
-          .addEventListener("click", function () {
-            // buildingName'i alın
-            buildingName = document.getElementById("buildingNameInput").value;
-            // Verileri console.log ile gösterin
-            console.log("clickedCoordinate:", clickedCoordinate);
-            console.log("buildingName:", buildingName);
-
-            //Add Marker
-            var marker = new ol.Feature({
-              geometry: new ol.geom.Point(ol.proj.fromLonLat(clickedLonLat)),
-            });
-
-            var markerStyle = new ol.style.Style({
-              image: new ol.style.Icon({
-                anchor: [0.5, 1],
-                src: "https://cdn.rawgit.com/openlayers/openlayers.github.io/master/en/v6.4.3/examples/data/icon.png",
-              }),
-            });
-
-            marker.setStyle(markerStyle);
-
-            var vectorSource = new ol.source.Vector({
-              features: [marker],
-            });
-
-            var vectorLayer = new ol.layer.Vector({
-              source: vectorSource,
-            });
-
-            map.addLayer(vectorLayer);
-            panel.close();
-          });
-      },
+  function addInteractions() {
+    draw = new ol.interaction.Draw({
+      source: source,
+      type: typeSelect.value,
     });
-  });
+
+    map.addInteraction(draw);
+    snap = new ol.interaction.Snap({ source: source });
+    map.addInteraction(snap);
+
+    draw.on("drawend", function (event) {
+      const feature = event.feature;
+      sourceFeature = event.feature;
+      const featureType = feature.getGeometry().getType();
+
+      map.removeInteraction(draw);
+      map.removeInteraction(snap);
+
+      let buildingName = "";
+
+      const geometry = feature.getGeometry();
+      const coordinates = geometry.getCoordinates();
+      const [x, y] = coordinates;
+      var clickedLonLat = ol.proj.toLonLat(coordinates);
+
+      const panel = jsPanel.create({
+        theme: "primary",
+        borderRadius: "1rem",
+        boxShadow: 5,
+        content: `
+            <div class="form">
+              <div class="input-container">
+                <label>Langitude Value </label>
+                <input type="text" id="input1" placeholder="Input 1" readonly>
+              </div>
+              <div class="input-container">
+              <label>Latitude Value </label>
+                <input type="text" id="input2" placeholder="Input 2" readonly>
+              </div>
+              <div class="input-container">
+                <label>Enter Door Name</label>
+                <input type="text" id="buildingNameInput" >
+              </div>
+               <div class="buttonss">
+                 <div id="finishButton" class="b">Done</div>
+                 <div id="closePanelButton" class="b r">Exit</div>
+               </div>
+  
+            </div>
+        `,
+        contentSize: "700 550",
+
+        headerTitle: featureType,
+        position: "center",
+
+        callback: function () {
+          document.getElementById("input1").value = clickedLonLat[0];
+          document.getElementById("input2").value = clickedLonLat[1];
+
+          document
+            .getElementById("closePanelButton")
+            .addEventListener("click", function () {
+              source.removeFeature(sourceFeature);
+              panel.close();
+            });
+
+          document
+            .getElementById("finishButton")
+            .addEventListener("click", function () {
+              buildingName = document.getElementById("buildingNameInput").value;
+              console.log("clickedCoordinate:", clickedLonLat);
+              console.log("buildingName:", buildingName);
+              panel.close();
+            });
+        },
+      });
+    });
+  }
+
+  typeSelect.onchange = function () {
+    map.removeInteraction(draw);
+    map.removeInteraction(snap);
+    addInteractions();
+  };
+
+  addInteractions();
 });
 
 // Data Fetch
 
-const apiUrl = "http://localhost:5280/api/Door"; // API'nin URL'sini buraya girin
+const apiUrl = "http://localhost:5280/api/Door";
 var _incomingData = null;
 
 await fetch(apiUrl)
@@ -135,11 +150,6 @@ await fetch(apiUrl)
     console.error("Hata:", error);
   });
 
-console.log(_incomingData);
-_incomingData.forEach((element) => {
-  console.log(element.x);
-});
-
 const query = document.querySelector("#queryPointBtn");
 query.addEventListener("click", function () {
   jsPanel.create({
@@ -148,7 +158,6 @@ query.addEventListener("click", function () {
     contentSize: "800 550",
     content: `<table id="myTable" class="display">
   <thead>
-
       <tr>
           <th>ID</th>
           <th>X</th>
@@ -158,21 +167,43 @@ query.addEventListener("click", function () {
       </tr>
   </thead>
   <tbody>
-  
-      ${_incomingData.map(
-        (item) => `<tr>
-          <td>${item.id}</td>
-          <td>${item.x}</td>
-          <td>${item.y}</td>
-          <td><button>Güncelle</button></td>
-          <td><button>Sil</button></td>
-       </tr>`
-      )}
-  
-      
       
   </tbody>
 </table> `,
+    callback: function () {
+      const tableBody = document.querySelector("#myTable tbody");
+
+      _incomingData.map((item) => {
+        const row = document.createElement("tr");
+        const id = document.createElement("td");
+        const x = document.createElement("td");
+        const y = document.createElement("td");
+
+        id.textContent = item.id;
+        x.textContent = item.x;
+        y.textContent = item.y;
+
+        row.appendChild(id);
+        row.appendChild(x);
+        row.appendChild(y);
+
+        const updateCell = document.createElement("td");
+        const updateButton = document.createElement("button");
+        updateButton.textContent = "Update";
+        updateButton.classList.add("button", "update");
+        updateCell.appendChild(updateButton);
+        row.appendChild(updateCell);
+
+        const deleteCell = document.createElement("td");
+        const deleteButton = document.createElement("button");
+        deleteButton.textContent = "Delete";
+        deleteButton.classList.add("button", "delete");
+        deleteCell.appendChild(deleteButton);
+        row.appendChild(deleteCell);
+
+        tableBody.appendChild(row);
+      });
+      let table = new DataTable("#myTable");
+    },
   });
-  let table = new DataTable("#myTable");
 });
